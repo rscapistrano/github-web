c0fdb422651231eccb83df08a7481b2dbab794b0
Riva Capistrano <satina13@hotmail.com>
2014-06-13 21:12:25 -0400

    clean up code

--- scripts/sample-exercises.js
+++ scripts/sample-exercises.js
@@ -1,25 +1,24 @@
-/*
-JavaScript
-	- Weak typing
-
-Operators with =
-	= assignment
-	== equality (values can be equal, but type can be different)
-	=== strict equality  (values and types must be the same)
-*/
+/** JavaScript
+ * - Weak typing
+ *
+ * Operators with =
+ *	= assignment
+ *	== equality (values can be equal, but type can be different)
+ *	=== strict equality  (values and types must be the same)
+ */
 
 
 
 
 // Call Prompt in JS
-var name = prompt('What is your name?');
-alert('Hello, ' + name);
+var name = prompt('What is your name?')
+alert('Hello, ' + name)
 
 
 // Create Variables
 // Leaving out 'var' creates a global variable and future headaches
 
-var cat = 'Teega', colorFur = 'Undecided', favortiteCatFoor = 'Bif';
+var cat = 'Teega', colorFur = 'Undecided', favortiteCatFoor = 'Bif'
 
 
 
@@ -48,24 +47,25 @@ document.body.innerText = animal1.quote
 
 
 // Adding a property to an Object
-animal1.whereItLives = 'manhattan highrise';
+animal1.whereItLives = 'manhattan highrise'
 // or
-Animal1['whereItLives'] = 'manhattan highrise';
+Animal1['whereItLives'] = 'manhattan highrise'
 
 // Deleting a property to an Object
-delete animal1.color;
+delete animal1.color
 
 
 
 
 
-/* References
-	• This statement does not make the same copy of an object so changes to animal2 will apply to animal1.
-	• This gives you two objects that point to the same value. You will only run into this when dealing with objects.
-	• To copy an object, you will need to create another variable with the same properties.
-*/ 
-var animal2 = animal1;
+/**
+ * References
+ * This statement does not make the same copy of an object so changes to animal2 will apply to animal1.
+ * This gives you two objects that point to the same value. You will only run into this when dealing with objects.
+ * To copy an object, you will need to create another variable with the same properties.
+ */
 
+var animal2 = animal1
 
 
 
@@ -76,53 +76,53 @@ var string1 = 'this is the best video'
 var string2 = 'i love this video'
 var string3 = 'it is the best ever'
 
-var regex = /this/;
+var regex = /this/
 
-console.log( regex.test(string1) );
-console.log( regex.test(string2) );
-console.log( regex.test(string3) );
-
-
+console.log( regex.test(string1) )
+console.log( regex.test(string2) )
+console.log( regex.test(string3) )
 
 
 
-/* CONDITIONAL CODE
 
-{ braces - code block }
 
-if ( condition ) {
-	your conditional code goes here..
-}
-*/
+/**
+ * CONDITIONAL CODE
+ * { braces - code block }
+ * if ( condition ) {
+ * 		your conditional code goes here..
+ * }
+ */
+
 
 // Simple If Statement
-var a = 5;
-var b = 10;
+var a = 5
+var b = 10
 
 if ( a < b ) {
-	alert('Yes, a is less than b.');
+	alert('Yes, a is less than b.')
 }
 
 if ( a > b ) {
-	alert('Yes, a is greater than b.');
+	alert('Yes, a is greater than b.')
 }
 
 if ( a === b ) {
-	alert('a and b are exactly equal');
+	alert('a and b are exactly equal')
 }
 
 
 
 // Complex Conditions
 
-var balance = 0;
+var balance = 0
 
 if ( balance > 0 ) {
 
-	alert('The balance is positive');
+	alert('The balance is positive')
 
 } else {
-	alert('Panic attack coming in 3, 2, 1...');
+	alert('Panic attack coming in 3, 2, 1...')
 }
 
 
@@ -131,69 +131,69 @@ if ( balance > 0 ) {
 
 // Conditional Ifs
 
-var	answer = window.promt('Type YES, NO, or MAYBE. Then click Ok.');
+var	answer = window.promt('Type YES, NO, or MAYBE. Then click Ok.')
 
-	if (answer === 'YES') { console.log('You brown noser!'); }
+	if (answer === 'YES') { console.log('You brown noser!') }
 
-	else if (answer === 'NO') { console.log('How dare you deny me!'); }
+	else if (answer === 'NO') { console.log('How dare you deny me!') }
 
-	else if (answer === 'MAYBE') { console.log('What the fuck! Make up your mind!'); }
+	else if (answer === 'MAYBE') { console.log('What the fuck! Make up your mind!') }
 
-	else { console.log('This game is over.'); }
-
-
+	else { console.log('This game is over.') }
 
 
 
-/* Conditionals Switch
-	This is best used when you have several cases.
-*/ 
 
-var gasGrade = 'Premium';
+
+/**
+ * Conditionals Switch
+ * This is best used when you have several cases.
+ */
+
+
+var gasGrade = 'Premium'
 
 switch ( gasGrade ) {
 	case 'Regular' :
-			alert('It is $3.15');
-			break;   // Jump us out of the switch statement and move on to the next.
+			alert('It is $3.15')
+			break   // Jump us out of the switch statement and move on to the next.
 
 	case 'Premium' :
-			alert('It is $3.35');
-			break;
+			alert('It is $3.35')
+			break
 
 	case 'Diesel'  :
-			alert('It is $3.47');
-			break;
+			alert('It is $3.47')
+			break
 
 	default        :
-			alert('Unknown gas grade');
-			break;    // Not necessary because we are at the very end.
+			alert('Unknown gas grade')
+			break    // Not necessary because we are at the very end.
 }
 
 
 
 
-var	answer = window.promt('Type YES, NO, or MAYBE. Then click Ok.');
+var	answer = window.promt('Type YES, NO, or MAYBE. Then click Ok.')
 
 Switch (answer) {
 
 	case 'YES' :
-		console.log('You brown noser!');
-		break;
+		console.log('You brown noser!')
+		break
 
 	case 'MAYBE' :
-		console.log('What the fuck! Make up your mind!');
-		break;
+		console.log('What the fuck! Make up your mind!')
+		break
 
 	case 'NO' :
-		console.log('How dare you deny me!');
-		break;
+		console.log('How dare you deny me!')
+		break
 
 	default :
-		console.log('This game is over.');
-		break;
+		console.log('This game is over.')
+		break
 }
-
-
 
 
 
@@ -202,10 +202,10 @@ Switch (answer) {
 
 // Terse Ifs: One-liners
 
-var	cherub = 'Cupid';
-  // cherub = 'Some other guy';
+var	cherub = 'Cupid'
+  // cherub = 'Some other guy'
 
-if (cherub === 'Cupid') console.log('Ouch, an arrow! Oh, I\'m in love!');
+if (cherub === 'Cupid') console.log('Ouch, an arrow! Oh, I\'m in love!')
 
 
 
@@ -213,12 +213,12 @@ if (cherub === 'Cupid') console.log('Ouch, an arrow! Oh, I\'m in love!');
 
 // Terse Ifs: Ternary Operator
 
-var animal = 'cat';
-// animal = 'dog';
+var animal = 'cat'
+// animal = 'dog'
 
-animal === 'cat' ? console.log('You will be a cat herder') : console.log('You will be a dog catcher');
+animal === 'cat' ? console.log('You will be a cat herder') : console.log('You will be a dog catcher')
 
-// var job = (animal === 'cat' ? 'cat herder' : 'dog catcher');
+// var job = (animal === 'cat' ? 'cat herder' : 'dog catcher')
 
 
 
@@ -226,23 +226,23 @@ animal === 'cat' ? console.log('You will be a cat herder') : console.log('You wi
 
 // For Loops - Sequencial
 
-for (var i = 0; i < 100; i ++ 1) {
-	console.log(i);
+for (var i = 0 i < 100 i ++ 1) {
+	console.log(i)
 }
 
 
 
 
 
-// Common use case; looping over an array
+// Common use case looping over an array
 
-var pageNames = ['Home', 'About Us', 'Contact Us', 'JavaScript Playground', 'News', 'Blog'];
+var pageNames = ['Home', 'About Us', 'Contact Us', 'JavaScript Playground', 'News', 'Blog']
 
-for (i = 0; i < pageNames.length; i+= 1) {
+for (i = 0 i < pageNames.length i+= 1) {
 	if (document.title === pageNames[i]) {
-		console.log('We are here: ' + pageNames[i]);
+		console.log('We are here: ' + pageNames[i])
 	} else {
-		console.log('We are not here: ' + pageNames[i]);
+		console.log('We are not here: ' + pageNames[i])
 	}
 }
 
@@ -251,15 +251,15 @@ for (i = 0; i < pageNames.length; i+= 1) {
 
 
 // Replace redundancy
-var pageNames = ['Home', 'About Us', 'Contact Us', 'JavaScript Playground', 'News', 'Blog'];
+var pageNames = ['Home', 'About Us', 'Contact Us', 'JavaScript Playground', 'News', 'Blog']
 
-for (i = 0; i < pageNames.length; i+= 1) {
-	var currentPageTitle = pageNames[i];
+for (i = 0 i < pageNames.length i+= 1) {
+	var currentPageTitle = pageNames[i]
 
 	if (document.title === currentPageTitle) {
-		console.log('We are here: ' + currentPageTitle);
+		console.log('We are here: ' + currentPageTitle)
 	} else {
-		console.log('We are not here: ' + currentPageTitle);
+		console.log('We are not here: ' + currentPageTitle)
 	}
 }
 
@@ -267,18 +267,19 @@ for (i = 0; i < pageNames.length; i+= 1) {
 
 
 
-/* For Loops - Enumerative
-   	Used for arrays and objects
-	It's NOT adviced to use a 'for in' loop for an array since in an array, the order is important
-	You can use it on other objects.
-*/
+/**
+ * For Loops - Enumerative
+ * Used for arrays and objects
+ * It's NOT adviced to use a 'for in' loop for an array since in an array, the order is important
+ * You can use it on other objects.
+ */
 
 // iterate over an array
 
-var pageNames = ['Home', 'About Us', 'Contact Us', 'JavaScript Playground', 'News', 'Blog'];
+var pageNames = ['Home', 'About Us', 'Contact Us', 'JavaScript Playground', 'News', 'Blog']
 
 for (var p in pageNames) {
-	console.log(p + ' is ' + pageNames[p]);
+	console.log(p + ' is ' + pageNames[p])
 }
 
 
@@ -290,11 +291,11 @@ var pages = {
 	third  : 'Contact Us',
 	fourth : 'JavaScript Playground',
 	fifth  : 'Blog'
-};
+}
 
 for (var p in pages) {
 	if (pages.hasOwnProperty(p)) {
-		console.log(p + ' is ' + pages[p]);
+		console.log(p + ' is ' + pages[p])
 	}
 }
 
@@ -303,45 +304,52 @@ for (var p in pages) {
 
 
 
-/* WHILE LOOPS
-When do you use a while loop?
-When you don't know how many times you need to loop over something. Don't know the end point.
-*/
+/**
+ * WHILE LOOPS
+ * When do you use a while loop?
+ * When you don't know how many times you need to loop over something. Don't know the end point.
+ */
 
-var i = 0;
+
+
+var i = 0
 while (i < 10) {
-	console.log(i + '... this will go until we hit 10.');
-	i += 1;
+	console.log(i + '... this will go until we hit 10.')
+	i += 1
 }
 
 // Another example
 var myArray = [true, true, true, false, true, true]
 
-var myItem = null;
+var myItem = null
 while (myItem !== false) {
-	console.log('myArray has ' + myArray.length + ' items now. this loop will go on until we pop a false.');
-	myItem = myArray.pop();
+	console.log('myArray has ' + myArray.length + ' items now. this loop will go on until we pop a false.')
+	myItem = myArray.pop()
 }
 
-/* DO WHILE LOOP
-	If you set myItem to one of the values in the array, it will not execute
-	In this case, use a 'do while loop' so the code gets executed at least once
-*/ 
-var myArray = [true, true, true, false, true, true];
 
-var myItem = false;
+/**
+ * DO WHILE LOOP
+ * If you set myItem to one of the values in the array, it will not execute
+ * In this case, use a 'do while loop' so the code gets executed at least once
+ */
+	
+ 
+var myArray = [true, true, true, false, true, true]
+
+var myItem = false
   do {
-	console.log('myArray has ' + myArray.length + ' items now. this loop will go on until we pop a false.');
-	myItem = myArray.pop();
-} while (myItem !== false);
+	console.log('myArray has ' + myArray.length + ' items now. this loop will go on until we pop a false.')
+	myItem = myArray.pop()
+} while (myItem !== false)
 
 
 // How to lock your web browser with an infinite loop...just for fun.
-counter = 1;
+counter = 1
 while (true) {
-	console.log(counter);
-	counter ++;
-	break; 		// Comment out the break for funtimes. 
+	console.log(counter)
+	counter ++
+	break 		// Comment out the break for funtimes. 
 				// Ok, that was fun. Don't do that again.
 }
 
@@ -349,100 +357,130 @@ while (true) {
 
 
 
-/* BASIC FUNCTIONS
-	Functions are all objects in JavaScript
-	You can pass functions into other functions; they can be passed around as values
-*/
+/**
+ * BASIC FUNCTIONS
+ * Functions are all objects in JavaScript
+ * You can pass functions into other functions they can be passed around as values
+ */
+	
 
 // Things that Teega likes to do
 
-console.log('bite');
-console.log('eat bif');
-console.log('chase flies');
-console.log('look cute for bif and sleep');
+console.log('bite')
+console.log('eat bif')
+console.log('chase flies')
+console.log('look cute for bif and sleep')
 
 // Create a function to wrap up those 4 lines above so I can use them over and over again.
 
 function teegaActivities() {
-	console.log('bite');
-	console.log('eat bif');
-	console.log('chase flies');
-	console.log('look cute for bif and sleep');
+	console.log('bite')
+	console.log('eat bif')
+	console.log('chase flies')
+	console.log('look cute for bif and sleep')
 }
 
 // Invoke the function
-teegaActivities();
+teegaActivities()
 
 
 // Case 1: Time for something real that we can do with functions - Modify Data
 
+/**
+ * Replaces r and g with w
+ * @param  {string} speech
+ * @return {string}
+ */
+
 function fuddify(speech) {
 	// If it's not a string, return an error message
-	if (typeof speech	!== 'string') return 'Nice twy, wabbit!';
+	if (typeof speech	!== 'string') return 'Nice twy, wabbit!'
 
 	// Otherwise, make it sound like Elmer Fudd
-	speech = speech.replace(/r/g, 'w');
-	speech - speech.replace(/R/g, 'w');
-	return speech;
+	speech = speech.replace(/r/g, 'w')
+	speech - speech.replace(/R/g, 'w')
+	return speech
 }
 
 // Process a string immediately and assign it to a variable
 var utterance = fuddify('You screwy rabbit.')
 
 
+
 // Case 2: Answer to a question that we ask over and over again
 
+/**
+ * Identify if a number is odd
+ * @param  {num}  num
+ * @return {Boolean}
+ */
 function isOdd(num) {
 	if (num % 2 === 0) {
-		return false;
+		return false
 	} else {
-		return true;
+		return true
 	}
 }
+
+/**
+ * Identify if number is even
+ * @param  {num}  num
+ * @return {Boolean}
+ */
 
 function isEven(num) {
 	if (num % 2 !== 0) {
-		return false;
+		return false
 	} else {
-		return true;
+		return true
 	}
 }
 
 
 
-// Arguments and Functions: What to say and how many times to say it?
+/**
+ * Arguments and Functions: What to say and how many times to say it?
+ * This pattern works for default values
+ * Check if the argument is undefined, and if it is, provide a default values
+ * @param  {[string]} sayWhat
+ * @param  {[number]} howMany
+ * @return {[type]}
+ */
 
 function saySomething(sayWhat, howMany) {
-	// This pattern works for default values
-	// Check if the argument is undefined, and if it is, provide a default value
 
-	var sayWhat = (typeof sayWhat !== 'undefined') ? sayWhat : 'Blah, blah, blah...';
-	var howMany = (typeof howMany !== 'undefined') ? howMany : 10;
+	var sayWhat = (typeof sayWhat !== 'undefined') ? sayWhat : 'Blah, blah, blah...'
+	var howMany = (typeof howMany !== 'undefined') ? howMany : 10
 
-	for (var i = 0, i < howMany; i += 1) {
-		console.log(sayWhat + " (" + i + ")");
+	for (var i = 0, i < howMany i += 1) {
+		console.log(sayWhat + " (" + i + ")")
 	}
 }
 
-// Adding Machine
+
+
+/**
+ * It adds all the entered inputs
+ * @return {number}
+ */
 
 function addingMachine() {
 	// Initialize the total we'll be returning
-	var total = 0;
+	var total = 0
 
-	for (var i = 0; i < arguments.length; i += 1) {
+	for (var i = 0 i < arguments.length i += 1) {
 		// Grab the next number
-		var number	= arguments[i];
+		var number	= arguments[i]
 
 		// Check if the argument is a number
 		// If so, add it to the running total
 		if (typeof number === 'number') {
-			total += number;
+			total += number
 		}
 	}
 
 	// Done - Return the total
-	return total;
+	return total
 }
 
 // Invoke the function
@@ -453,26 +491,56 @@ addingMachine (1,2,12,45,20)
 
 
 // Understanding Variable Scope
+/**
+ * Random number
+ * @type {Number}
+ */
+var myNum = 32
 
-var myNum = 32;
-var myResult = 'Success';
+/**
+ * This is my return text
+ * @type {String}
+ */
+var myResult = 'Success'
+
 
 function randomizer(limit) {
 
-	var myNum = Math.floor(Math.random() * limit);
+	var myNum = Math.floor(Math.random() * limit)
 
-	console.log('myNum is ' + myNum);
-	console.log('Global myNum is ' + window.myNum);
+	console.log('myNum is ' + myNum)
+	console.log('Global myNum is ' + window.myNum)
 
-	return myNum;
+	return myNum
 }
 
 
+/**
+ * Tells me the value of a variable
+ * @return {num}
+ */
+function myFunction () {
+	var a = 5
+	var b = 10
+	var c = 20
+	var d = a + b + c
+	alert( 'The value of d is: ' + d )
+}
+
+// Call the function
+myFunction()
 
 
 
+/**
+ * Setting parameters and arguments
+ */
 
+function addTwoNumbers(num1, num2) {
+	var result = num1 + num2
+	alert(result)
+}
 
-
+addTwoNumbers (5,25)
 
 
